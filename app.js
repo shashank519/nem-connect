3c9600f4a84dccef9f5ea4b7b0204ce87c0090a6
Shashank Dave <shashank.dave@asticsinc.com>
2018-04-30 19:15:12 +0530

    passport

--- app.js
+++ app.js
@@ -1,47 +1,63 @@
-var express = require("express");
-var path = require("path");
-var favicon = require("serve-favicon");
-var logger = require("morgan");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 // var jwt = require('jsonwebtoken');
-var passport = require("passport");
-var passportJWT = require("passport-jwt");
+var passport = require('passport');
+var passportJWT = require('passport-jwt');
 
-var index = require("./routes/index");
-var users = require("./routes/users");
+var index = require('./routes/index');
+var users = require('./routes/users');
+var Strategy = require('passport-http-bearer').Strategy;
 
 var ExtractJwt = passportJWT.ExtractJwt;
 var JwtStrategy = passportJWT.Strategy;
 
-var dbConnect = require("./db.connect.js");
+var findIfAnyUserExists = require('./src/models/registration').findIfUserExists;
 
-dbConnect.connect().then(function(res, err) {
-  if (err) {
-    console.log("Error occured in connection with Mongo DB");
-  } else if (res) {
-    console.log("Connection with mongodb successful.");
-  }
+
+var dbConnect = require('./db.connect.js');
+
+// console.log(dbConnect);
+
+dbConnect.connect().then(function(res, err){
+	if(err) {console.log('Error occured in connection with Mongo DB');}
+	else if(res){ console.log('Connection with mongodb successful.')}
 });
 
 var app = express();
 
 var jwtOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: "shashankdave"
+  secretOrKey: 'shashankdave'
 };
 
-var strategy = new JwtStrategy(jwtOptions, function(jwt_payload, done) {
-  console.log("Jwt received", jwt_payload);
-  var user = true;
-  if (user) {
-    done(null, user);
-  } else {
-    done(null, false);
-  }
-});
+// var strategy = new JwtStrategy(jwtOptions, function(jwt_payload, done){
+//   console.log('Jwt received', jwt_payload);
 
-passport.use(strategy);
+//   var user = true;
+//   if(user){
+//     done(null, user);
+//   } else {
+//     done(null, false);
+//   }
+// })
+
+// passport.use(strategy);
+
+passport.use(new Strategy(jwtOptions, function(token, cb) {
+    console.log('tokenddd', token);
+    findIfAnyUserExists({email: 'shashank@gmail.com'}).then(function(response, err){
+      if (err) { return cb(err); } 
+      else if(!response) { return cb(null, false);} 
+      return cb(null, response);
+    }).catch(function(e){
+      return cb(null, {'message': 'erroree'})
+    })
+  }
+));
 
 app.use(passport.initialize());
 
@@ -51,27 +67,24 @@ app.use(passport.initialize());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.use("/", index);
-app.use("/users", users);
+app.use('/', index);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error("Not Found");
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -80,11 +93,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.send("error");
+  res.send('error');
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,80 +1,65 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var jwt = require("jsonwebtoken");
-var passportJWT = require("passport-jwt");
+var jwt = require('jsonwebtoken');
+var passportJWT = require('passport-jwt');
+var passport = require('passport');
 
 var ExtractJwt = passportJWT.ExtractJwt;
 
-var registerationNewUser = require("../src/models/registration").registerUser;
-var findIfAnyUserExists = require("../src/models/registration")
-  .findIfUserExists;
+var registerationNewUser = require('../src/models/registration').registerUser;
+var findIfAnyUserExists = require('../src/models/registration').findIfUserExists;
 
 var jwtOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: "shashankdave"
-};
+  secretOrKey: 'shashankdave'
+}
 /* GET users listing. */
-router.get("/", function(req, res, next) {
-  res.send("respond with a resource");
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
-router.post("/register", function(req, resp) {
-  findIfAnyUserExists(req.body)
-    .then(function(response, err) {
-      if (err) {
-        resp.json(err);
-      } else {
-        if (
-          response &&
-          response.email.length &&
-          response.email === req.body.email
-        ) {
-          resp.send({
-            message: "User alreasy exists. Try with another email.",
-            errorWithSaving: "User already exists."
-          });
-        } else {
-          registerationNewUser(req.body).then(function(respn, err) {
-            if (err) {
-              resp.json(err);
-            } else {
-              resp.json({
-                response: respn,
-                message: "User registered successfully."
-              });
-            }
-          });
-        }
-      }
-    })
-    .catch(function(e) {
-      throw e;
-    });
-  // registerationUser(req.body).then(function(err, response){
-  // 	if(err) {
-  // 		resp.json(err);
-  // 	} else {
-  // 		resp.json(response);
-  // 	}
-  // })
+router.post('/register', function(req, resp){
+	findIfAnyUserExists(req.body).then(function(response, err){
+		console.log(response);
+		if(err) { resp.json(err)}
+		else {
+			if(response && response.email.length && response.email === req.body.email){
+				resp.send({'message': 'User alreasy exists. Try with another email.', errorWithSaving: 'User already exists.'})
+			} else {
+				registerationNewUser(req.body).then(function(response, err){
+					if(err) {
+						resp.json(err);
+					} else {
+						resp.json({'response': response, 'message': 'User registered successfully.'});
+					}
+				})
+			}
+		}
+	}).catch(function(e){
+		throw e;
+	})
 });
 
-router.post("/login", function(req, resp) {
-  if (req.body.email && req.body.password) {
-    var email = req.body.email;
-    var password = req.body.password;
-  }
-  var user = {
-    email: "shashank@gmail.com",
-    password: "shashankdave"
-  };
-  if (user.email === "shashank@gmail.com") {
-    var payload = { id: "12345" };
-    var token = jwt.sign(payload, jwtOptions.secretOrKey);
-    resp.json({ msg: "ok", token: token });
-  } else {
-    resp.status(401).json({ message: "passwords did not match" });
-  }
-});
+router.post('/login', function(req, resp){
+	findIfAnyUserExists(req.body).then(function(response, err){
+		if (err){
+			resp.json({err: err})
+		} else {
+			if(req.body.email === response.email){
+				var payload = {email: response.email};
+				var token = jwt.sign(payload, jwtOptions.secretOrKey);
+				resp.json({msg: 'ok', token: token});
+			} else {
+				resp.status(401).json({'message': 'passwords did not match'});
+			}
+		}
+	}).catch(function(e){
+		resp.status(401).json({'message': 'Wrong credentials'})
+	})
+})
+
+router.post('/secret', passport.authenticate('bearer', { session: false }), function(req, resp){
+	resp.json({ 'Hello': 'Hello world' });
+})
 
 module.exports = router;
